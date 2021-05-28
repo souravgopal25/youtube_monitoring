57352c4cfb1c18962f125f0410a7b241a3e5a047
Sourav <souravgopal25@gmail.com>
2021-05-28 15:43:58 +0530

    youtube.go added

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
 
-func main()  {
+func setupRoutes() {
+	http.HandleFunc("/", homepage)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func homepage(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(writer, "homepage ")
+
+}
+
+func main() {
 	fmt.Println("Hello World")
+	//setupRoutes
+	//var GCPKEY=[]byte(os.Getenv("GCPAPI"))
+
+	fmt.Println([]byte(os.Getenv("GCPAPI")))
 }
